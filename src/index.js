52cc6d269f1bf610e6a5c1b990234cb61976e344
Patrick Nepp <ptnepp@gmail.com>
2022-09-11 01:09:02 -0500

    global theme

--- src/index.js
+++ src/index.js
@@ -5,18 +5,23 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import { mainNavbarItems } from './components/Navbar/const/navbarItems';
+import { ThemeProvider } from '@mui/material';
+import { dashboardTheme } from './dashboardTheme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-  <Routes>
-    <Route path='/' element={<App />}>
-      {mainNavbarItems.map((item) => {
-        return <Route key={item.id} path={item.route} element={item.element} />
-      })}
-    </Route>
-  </Routes>
-  </BrowserRouter>
+  <ThemeProvider theme={dashboardTheme}>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />}>
+        {mainNavbarItems.map((item) => {
+          return <Route key={item.id} path={item.route} element={item.element} />
+        })}
+      </Route>
+    </Routes>
+    </BrowserRouter>
+  </ThemeProvider>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/Authentication/Authentication.js
+++ src/pages/Authentication/Authentication.js
@@ -1,10 +1,44 @@
 import React from 'react'
-import { Grid } from '@mui/material'
+import Grid from '@mui/material/Grid';
+import CommonButton from '../../components/common/CommonButton/CommonButton';
 
 const Authentication = () => {
-  return (
-    <Grid item xs={8}>Authentication</Grid>
-  )
+    const buttonStyles = {
+        fontSize: '0.875rem',
+        fontWeight: 600,
+        textTransform: 'capitalize',
+        borderRadius: 2.5,
+        '&.MuiButton-contained': {
+            backgroundColor: '#009be5',
+            '&:hover': {
+                backgroundColor: '#006db3'
+            },
+        },
+        '&.MuiButton-outlined': {
+            color: "#fff",
+            borderColor: '#fff',
+            '&:hover': {
+                backgroundColor: 'transparent'
+            },
+        },
+    };
+
+    return (
+        <Grid item xs={8}>
+            <CommonButton
+                sx={buttonStyles}
+                variant="contained"
+            >
+                Add user
+            </CommonButton>
+            <CommonButton
+                sx={buttonStyles}
+                variant="contained"
+            >
+                web setup
+            </CommonButton>
+        </Grid>
+    )
 }
 
 export default Authentication
